b87c2cd669377bfed2fc10d0ed0403739275aed4
hfzwhd <hafizwahid.e@gmail.com>
2017-08-17 15:51:39 +0800

    2nd try commit

--- learngit/ViewController.swift
+++ learngit/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         print("lambo")
         print("git kraken shitt")
         print("kraken")
+        print("second try")
     }
 
     override func didReceiveMemoryWarning() {
